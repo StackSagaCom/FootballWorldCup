e1508b1b29d5ec494e97afeaa155d872ba2af31d
StackSaga <adiosy@stacksaga.com>
2023-11-25 14:22:27 +0100

    Creating the console menu

--- ScoreBoard/Program.cs
+++ ScoreBoard/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ScoreBoard;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        FootballWorldCupScoreBoard scoreBoard = new FootballWorldCupScoreBoard();
+
+        Console.WriteLine("Welcome to the Football World Cup Score Board!");
+
+        while (true)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Start a game");
+            Console.WriteLine("2. Update score");
+            Console.WriteLine("3. Finish game");
+            Console.WriteLine("4. Display current state");
+            Console.WriteLine("5. Exit");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        // Start a game
+                        Console.Write("Enter the name of the home team: ");
+                        string homeTeam = Console.ReadLine();
+
+                        Console.Write("Enter the name of the away team: ");
+                        string awayTeam = Console.ReadLine();
+
+                        if (homeTeam.ToLower() != awayTeam.ToLower())
+                        {
+                            scoreBoard.StartGame(homeTeam, awayTeam);
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("For a football game, we would need two different teams. Please, try again with correct team names \n");
+                        }
+                        break;
+
+                    case 2:
+                        // Update score
+
+                    case 3:
+                        // Finish game
+
+                    case 4:
+                        // Display current state
+                        Console.WriteLine("Current state of the score board:");
+                        DisplayScoreBoard(scoreBoard);
+                        Console.WriteLine();
+                        break;
+
+                    case 5:
+                        // Exit
+                        Console.WriteLine("Exiting the program.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid option. Please choose a valid option.\n");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+
+    private static void DisplayScoreBoard(FootballWorldCupScoreBoard scoreBoard)
+    {
+        var summary = scoreBoard.GetSummaryByTotalScore();
+        foreach (var match in summary)
+        {
+            Console.WriteLine($"{match.HomeTeam} {match.HomeTeamScore} - {match.AwayTeamScore} {match.AwayTeam}");
+        }
+    }
+}
